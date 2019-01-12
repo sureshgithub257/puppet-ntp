655fc236fdd5235a51eb9cb2067d76467dbc975b
root <root@sureshgk3c.mylabserver.com>
2019-01-12 21:49:53 +0000

    Inital commit

---
+++ manifests/init.pp
@@ -0,0 +1,15 @@
+class ntp(
+  String $package_name        = $ntp::params::package_name,
+  String $package_ensure      = $ntp::params::package_ensure,
+  String $config_name         = $ntp::params::config_name,
+  String $config_file_mode    = $ntp::params::config_file_mode,
+  Array[String] $servers      = $ntp::params::servers,
+  String $service_name        = $ntp::params::service_name,
+  Boolean $service_enable     = $ntp::params::service_enable,
+  Boolean $service_hasstatus  = $ntp::params::service_hasstatus,
+  Boolean $service_hasrestart = $ntp::params::service_hasrestart,
+) inherits ::ntp::params {
+  class { '::ntp::install': }
+  -> class { '::ntp::config': }
+  ~> class { '::ntp::service': }
+}
